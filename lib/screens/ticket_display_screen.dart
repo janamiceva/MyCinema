44568969a5ca6dd5032fe1a2d085d1e002d8c4af
matejapetkovska <matejapetkovska2@gmail.com>
2023-04-16 19:56:53 +0200

    Modifying tickets display screen

--- lib/screens/ticket_display_screen.dart
+++ lib/screens/ticket_display_screen.dart
@@ -14,10 +14,10 @@ class TicketDisplayPage extends StatefulWidget {
 }
 
 class _TicketDisplayPageState extends State<TicketDisplayPage> {
-  Map<String, dynamic> latestTicket = {};
+  List<Map<String, dynamic>> tickets = [];
 
   @override
-  void initState() {
+   void initState() {
     super.initState();
     // Call your function here
     Future.delayed(Duration.zero, () async {
@@ -25,32 +25,32 @@ class _TicketDisplayPageState extends State<TicketDisplayPage> {
     });
   }
   
-  Future<void> getTickets() async{
+   Future<void> getTickets() async{
     final currentUser = FirebaseAuth.instance.currentUser;
-final email = currentUser.email;
-if (email == null) {
-  print('Error');
-}
-final querySnapshot = await FirebaseFirestore.instance
-  .collection('users')
-  .doc(FirebaseAuth.instance.currentUser.uid)
-  .collection('tickets')
-  .orderBy('date', descending: true)
-  .limit(1)
-  .get();
-setState(() {
+    final email = currentUser.email;
+    if (email == null) {
+      print('Error');
+    }
+    final querySnapshot = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(FirebaseAuth.instance.currentUser.uid)
+        .collection('tickets')
+        .orderBy('date', descending: true)
+        .get();
+    setState(() {
       if (querySnapshot.docs.isNotEmpty) {
-        latestTicket = querySnapshot.docs.first.data();
+        tickets = querySnapshot.docs.map((doc) => doc.data()).toList();
       } else {
-        latestTicket = null;
+        tickets = [];
       }
     });
   }
+
   @override
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     
-if (latestTicket == null) {
+if (tickets.isEmpty) {
     return Scaffold(
       backgroundColor: Color(0xffba68c8),
       appBar: AppBar(
@@ -120,120 +120,141 @@ if (latestTicket == null) {
             SizedBox(width: 15.0),
           ],
         ),
-      body: SafeArea(
-        child: SingleChildScrollView(
-        child: Stack(
-          children: [
-            Padding(
-              padding: const EdgeInsets.all(30.0),
-              child: Container(
-                height: size.height,
-                width: size.width,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(15.0),
-                ),
-                child: Column(
-                  children: [
-                    Container(
-                      height: size.height * .45,
-                      decoration: BoxDecoration(
-                          borderRadius:
-                              BorderRadius.vertical(top: Radius.circular(15.0)),
-                          image: DecorationImage(
-                              fit: BoxFit.cover,
-                              image: AssetImage(latestTicket['image']))),
-                    ),
-                    SizedBox(height: 20.0),
-                    Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Column(
-                            children: [
-                              TextFrave(
-                                  text: 'DATE:',
-                                  color: Colors.grey,
-                                  fontSize: 16),
-                              TextFrave(text: latestTicket['date']),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              TextFrave(
-                                  text: 'TICKETS',
-                                  color: Colors.grey,
-                                  fontSize: 16),
-                              TextFrave(
-                                text: latestTicket['tickets'].toString(),
-                                )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    Padding(
-                      padding: EdgeInsets.all(10.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Column(
-                            children: [
-                              TextFrave(
-                                  text: 'TIME: ',
-                                  color: Colors.grey,
-                                  fontSize: 16),
-                              TextFrave(text: latestTicket['time']),
-                            ],
-                          ),
-                          Column(
-                            children: [
-                              TextFrave(
-                                  text: 'SEATS:',
-                                  color: Colors.grey,
-                                  fontSize: 16),
-                              Row(
-                                children: List.generate(
-                                    latestTicket['selectedSeats'].length, (i) {
-                                  return TextFrave(
-                                      text:
-                                          '${latestTicket['selectedSeats']}'.toString());
-                                }),
-                              )
-                            ],
-                          ),
-                        ],
-                      ),
-                    ),
-                    SizedBox(height: 20.0),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: List.generate(31,
-                          (index) => TextFrave(text: '- ', color: Colors.grey)),
-                    ),
-                    SizedBox(height: 10.0),
-                    SizedBox(
-                      height: 120,
-                      width: 120,
-                      child: Image(image: AssetImage('images/qrcode.png')),
-                    )
-                  ],
-                ),
+     body: SafeArea(
+  child: SingleChildScrollView(
+    child: Container(
+      padding: const EdgeInsets.all(30.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: List.generate(
+          tickets.length,
+          (index) {
+            final ticket = tickets[index];
+            return Container(
+              height: size.height * 0.70,
+              margin: EdgeInsets.all(5),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(15.0),
               ),
-            ),
-            Positioned(
-                top: size.height * .695,
-                left: 15,
-                child: Icon(Icons.circle, color: Color(0xffba68c8))),
-            Positioned(
-                top: size.height * .695,
-                right: 15,
-                child: Icon(Icons.circle, color: Color(0xffba68c8))),
-          ],
-        ),
+              child: Column(
+                children: [
+                  Container(
+                    height: size.height * 0.25,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.vertical(top: Radius.circular(15.0)),
+                      image: DecorationImage(
+                        fit: BoxFit.cover,
+                        image: AssetImage(ticket['image']),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            TextFrave(
+                              text: 'DATE:',
+                              color: Colors.grey,
+                              fontSize: 16,
+                            ),
+                            TextFrave(
+                              text: ticket['date'],
+                            ),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            TextFrave(
+                              text: 'TICKETS',
+                              color: Colors.grey,
+                              fontSize: 16,
+                            ),
+                            TextFrave(
+                              text: ticket['tickets'].toString(),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(10.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            TextFrave(
+                              text: 'TIME: ',
+                              color: Colors.grey,
+                              fontSize: 16,
+                            ),
+                            TextFrave(
+                              text: ticket['time'],
+                            ),
+                          ],
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            TextFrave(
+                              text: 'SEATS:',
+                              color: Colors.grey,
+                              fontSize: 16,
+                            ),
+                            Row(
+                              children: List.generate(
+                                ticket['selectedSeats'].length,
+                                (i) {
+                                  return TextFrave(
+                                    text: '${ticket['selectedSeats'][i]} ',
+                                  );
+                                },
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 20.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: List.generate(
+                      31,
+                      (index) => TextFrave(
+                        text: '- ',
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 10.0),
+                  SizedBox(
+                    height: 120,
+                    width: 120,
+                    child: Image(
+                      image: AssetImage('images/qrcode.png'),
+                    ),
+                  ),
+             
+                ],
+              ),
+            );
+          },
         ),
       ),
+    ),
+  ),
+),
+
     );
   }
 }
